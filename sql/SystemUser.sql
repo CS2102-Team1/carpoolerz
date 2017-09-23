7971212d6058006e12c64ad36883ed55aa068e7c
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-09-23 17:42:03 +0800

    [SQL] Added insertion statements for systemuser

---
+++ sql/SystemUser.sql
@@ -0,0 +1,3 @@
+INSERT INTO systemuser (username, password, fullName, licenseNum) VALUES (‘user1’, ‘pass1’, ‘Derian Tungka’, ‘111’);
+INSERT INTO systemuser (username, password, fullName, licenseNum) VALUES (‘user2’, ‘pass2’, ‘Derian Tungka’, ‘222’);
+INSERT INTO systemuser (username, password, fullName, licenseNum) VALUES (‘user3’, ‘pass3’, ‘Derian Tungka’, ‘333’);
