af6cc38b34785a590e86f0eee35e3ce45e3221c2
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-09-23 17:47:04 +0800

    [SQL][FIX] Edited the punctuation marks

--- sql/Car.sql
+++ sql/Car.sql
@@ -1,3 +1,3 @@
-INSERT INTO car (numPlate, brand, model) VALUES (‘152’, ‘Toyota’, ‘Avanza’);
-INSERT INTO car (numPlate, brand, model) VALUES (‘121’, ‘Honda’, ‘Jazz’);
-INSERT INTO car (numPlate, brand, model) VALUES (‘341’, ‘Lamborghini’, ‘Aventador’);
+INSERT INTO car (numPlate, brand, model) VALUES ('152', 'Toyota', 'Avanza');
+INSERT INTO car (numPlate, brand, model) VALUES ('121', 'Honda', 'Jazz');
+INSERT INTO car (numPlate, brand, model) VALUES ('341', 'Lamborghini', 'Aventador');
