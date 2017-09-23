3d0a6f13686e2caa85068a2b6a0688ced22898ea
LeeYingZheng <LeeYingZheng@users.noreply.github.com>
2017-09-23 20:06:57 +0800

    Updated SQL Schema and Added TableValues file to insert values into tables

--- sql/Schema.sql
+++ sql/Schema.sql
@@ -3,12 +3,18 @@ DROP TABLE car;
 DROP TABLE ride;
 DROP TABLE user_bid;
 
+--Order of Implementation
+--1. car table
+--2. ride table
+--3. systemuser table
+--4. user_bid tabke
+
 CREATE TABLE systemuser (
     username VARCHAR(64) PRIMARY KEY,
     password VARCHAR(64) NOT NULL,
     fullName VARCHAR(64) NOT NULL,
     licenseNum VARCHAR(64) NOT NULL,
-    numPlate VARCHAR(64) SET DEFAULT NULL REFERENCES Car(numPlate) ON DELETE SET NULL ON UPDATE CASCADE
+    numPlate VARCHAR(64) DEFAULT NULL REFERENCES Car(numPlate) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE car (
@@ -17,21 +23,25 @@ CREATE TABLE car (
     model VARCHAR(64) NOT NULL
 );
 
+CREATE SEQUENCE ride_id;
 CREATE TABLE ride (
-    ride_id NUMERIC AUTO_INCREMENT PRIMARY KEY,
+    ride_id NUMERIC DEFAULT nextval('ride_id') PRIMARY KEY,
     startpoint VARCHAR(64) NOT NULL,
     endpoint VARCHAR(64) NOT NULL,
     starttime_date TIMESTAMP NOT NULL,
     endtime_date TIMESTAMP NOT NULL
 );
+ALTER SEQUENCE ride_id OWNED BY ride.ride_id;
 
 CREATE TABLE user_bid (
     bidAmount NUMERIC,
     success BOOLEAN,
     passgsrcdest VARCHAR(64),
     passgfindest VARCHAR(64),
+    username VARCHAR(64),
+    ride_id NUMERIC(64),
     FOREIGN KEY (username) REFERENCES systemuser (username),
-    FOREIGN KEY (ride_id) REFERENCES ride (ride_id),
+    FOREIGN KEY (ride_id)  REFERENCES ride (ride_id),
     PRIMARY KEY (username, ride_id)
 );
 
