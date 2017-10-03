606b740e46de71f41b0ab059f918b31a2e8ca1ab
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-03 21:39:17 +0800

    [LOGIN] Implemented basic login feature

---
+++ htdocs/users.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Carpoolerz</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link href="main.css" , rel="stylesheet" />
+</head>
+
+<body>
+    <h1>HELLO WORLD THIS IS THE USERS WEBSITE</h1>
+</body>
