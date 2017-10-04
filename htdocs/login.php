de80925af91af446b96db0449a315c2eb114a478
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-04 15:35:44 +0800

    [AUTHENTICATION] Added automatic redirect based on session stored information

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,13 +1,13 @@
 <?php
     session_start();
 
-    // if(isset($_SESSION['username'])!="") {
-    //     if ($_SESSION['usr_role'] === 1) {
-    //       header("Location: admin/");
-    //     } else {
-    //       header("Location: user/");
-    //     }
-    // }
+    if(isset($_SESSION['username'])!="") {
+        if ($_SESSION['username'] == 'admin@admin.com') {
+            header("Location: admin.php");
+        } else {
+            header("Location: users.php");
+        }
+    }
 
     $dbconn = pg_connect("host=localhost port=5432 dbname=carpoolerz user=postgres password=postgres")
                 or die('Could not connect: ' . pg_last_error());
@@ -19,23 +19,22 @@
 
         $query = "SELECT * FROM systemuser s WHERE s.username = $username AND s.password = $password";
         $result = pg_query($query);
-        // echo <p>$result</p>;
-
-        // while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-        //     echo "\t<tr>\n";
-        //     foreach ($line as $col_value) {
-        //         echo "\t\t<td>$col_value</td>\n";
-        //     }
-        //     echo "\t</tr>\n";
-        // }
 
         if (pg_num_rows($result) == 0) {
+
+            $_SESSION['username'] = $username;
+            $_SESSION['password'] = $password;
+
             ob_start();
-            header("Location: users.php");
+            if ($_SESSION['username'] == 'admin@admin.com') {
+                header("Location: admin.php");
+            } else {
+                header("Location: users.php");
+            }
             ob_end_flush();
             end();
         } else {
-            echo "\n Error. Cannot Log In... \n";
+            echo "\n Error. Incorrect Email or Password... \n";
         }
 
     } else {
