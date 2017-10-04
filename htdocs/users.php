c53d8a7d816bde0a71d5a0b40deefa35612341f3
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-04 17:15:15 +0800

    [STARTER] Added starter template to admin and user pages

--- htdocs/users.php
+++ htdocs/users.php
@@ -26,5 +26,58 @@
 </head>
 
 <body>
-    <h1>HELLO WORLD THIS IS THE USERS WEBSITE</h1>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">Carpoolerz</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    	<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="login.php">Logout</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
+    <div class=container>
+
+		<div class=container>
+			<!-- Display all current driver offered rides -->
+			<table class="table table-striped table-hover">
+				<thead class="thead-inverse">
+					<tr>
+						<th>Ride ID</th>
+						<th>Start Location</th>
+						<th>End Location</th>
+						<th>Start Time/Date</th>
+						<th>End Time/Date</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						$query = 'SELECT * FROM ride';
+						$result = pg_query($query);
+						while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+							echo "\t<tr>\n";
+							foreach ($line as $col_value) {
+								echo "\t\t<td>$col_value</td>\n";
+							}
+							echo "\t</tr>\n";
+						}
+					?>
+				</tbody>
+			</table>
+		</div>
+
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 </body>
