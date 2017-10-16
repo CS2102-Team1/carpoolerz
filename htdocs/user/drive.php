0b7fa8505f0c41ff62e8032cd5155c459971f3aa
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-16 19:16:54 +0800

    [DRIVE] Implemented create new ride offer functionality on Drive! page

--- htdocs/user/drive.php
+++ htdocs/user/drive.php
@@ -8,13 +8,21 @@
     $dbconn = pg_connect("host=localhost port=5432 dbname=carpoolerz user=postgres password=postgres")
     or die('Could not connect: ' . pg_last_error());
 
-    $query = /** @lang text */
+    $check_auth_query = /** @php text */
         "SELECT * FROM systemuser WHERE username = '$username' AND password = '$password' AND licensenum IS NOT NULL";
 
-    $result = pg_query($dbconn, $query);
+    $check_auth_result = pg_query($dbconn, $check_auth_query);
 
-    if (pg_num_rows($result) == 0) {
+    // Logic: If user has a car then he must be a driver since car updating happens in the driver-car page
+    $check_car_query = /** @php text */
+        "SELECT * FROM owns_car WHERE driver = '$username'";
+
+    if (pg_num_rows($check_auth_result) == 0) {
         header("Location: ../login.php");
+    }
+
+    if (pg_num_rows($check_car_query)) {
+        header("Location: ../drive-error.php");
     }
 ?>
 
@@ -25,38 +33,90 @@
     <?php include '../header.shtml'; ?>
 </head>
 
-<body>
-    <?php include 'navbar-user.shtml'; ?>
-    <div class=container>
-        <div class="container-fluid">
-            <h1 class="text-center">DRIVERS: CREATE NEW RIDE OFFER</h1>
-            <form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="login-form">
-                <div class="form-group">
-                    <label for="p_username">Username: </label>
-                    <h1><?php echo $username?></h1>
-                </div>
+    <body>
+        <?php include 'navbar-user.shtml'; ?>
+        <div class=container>
+            <div class="container-fluid">
+                <h1 class="text-center">RIDE OFFERS YOU MADE</h1>
+                <br/>
+                <h2>This is a stub</h2>
+                <br/>
+            </div>
 
-                <div class="form-group">
-                    <label for="p_password">Password: </label>
-                    <input type="password" name="p_password" required class="form-control" id="pwd" value="<?php echo $password?>" placeholder="Password"/>
-                </div>
+            <div class="container-fluid">
+                <h1 class="text-center">CREATE NEW RIDE OFFER</h1>
+                <form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="login-form">
+                    <div class="form-group">
+                        <label for="p_username">Username: </label>
+                        <h1><?php echo $username?></h1>
+                    </div>
 
-                <div class="form-group">
-                    <label for="p_fullname">Full Name: </label>
-                    <input type="text" name="p_fullname" required class="form-control" id="f_name" value="<?php echo $full_name?>" placeholder="Full Name"/>
-                </div>
+                    <div class="form-group">
+                        <label for="p_start">Starting Location: </label>
+                        <input type="text" name="p_start" required class="form-control" id="start_location" placeholder="Enter Starting Location"/>
+                    </div>
 
-                <div class="form-group">
-                    <label for="p_license">Driving License ID: </label>
-                    <input type="text" name="p_license" class="form-control" id="lic" value="<?php echo $license_id?>" placeholder="Driving License ID"/>
-                </div>
-                <button type="submit" name="changeProfileDetails" class="form-control btn btn-danger">UPDATE DETAILS</button>
-            </form>
+                    <div class="form-group">
+                        <label for="p_destination">Destination: </label>
+                        <input type="text" name="p_destination" required class="form-control" id="end_location" placeholder="Enter Destination"/>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="p_startDate">Start Date (DD/MM/YYYY): </label>
+                        <input type="text" name="p_startDate" class="form-control" id="start_date" placeholder="DD/MM/YYYY"/>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="p_startTime">Start Time (HH:mm:ss): </label>
+                        <input type="text" name="p_startTime" class="form-control" id="start_time" placeholder="HH:mm:ss"/>
+                    </div>
+
+                    <button type="submit" name="createNewRide" class="form-control btn btn-success">ADD RIDE OFFER</button>
+                </form>
+            </div>
         </div>
-    </div>
+        <?php include '../footer.shtml'; ?>
+    </body>
+    <?php
+        if (isset($_POST['createNewRide'])) {
+            $startAddress = $_POST['p_start'];
+            $endAddress = $_POST['p_destination'];
+            $startDate = $_POST['p_startDate'];
+            $startTime = $_POST['p_startTime'];
 
-    <?php include '../footer.shtml'; ?>
-</body>
+            $datetime = $startDate." ".$startTime;
 
+            //TODO: Condition for timestamp check is not working. See query below
+            $check_rides_query = /** @php text */
+                "SELECT * FROM ride WHERE driver = '$username' AND start_time = to_timestamp('$datetime', 'DD/MM/YYYY HH24:MI:SS') AND end_time ISNULL";
+
+            $check_rides_result = pg_query($dbconn, $check_rides_query);
+
+            if (pg_num_rows($check_rides_result) > 0) {
+                echo "<br/><h1 class='text-center'>You already have a ride scheduled for this time!</h1><br/><br/>";
+            } else {
+                $add_rides_query = /** @php text */
+                    "INSERT INTO ride(highest_bid, driver, from_address, to_address, start_time) VALUES(0, '$username', '$startAddress', '$endAddress', to_timestamp('$datetime', 'DD/MM/YYYY HH24:MI:SS'))";
+
+                $add_rides_query = pg_query($dbconn, $add_rides_query);
+
+                //Get Ride ID
+                $next_ride_id_query = /** @php text */
+                    "SELECT last_value FROM ride_id";
+
+                $next_ride_id_result = pg_query($dbconn, $next_ride_id_query);
+
+                $target_rideID = pg_fetch_row($next_ride_id_result)[0];
+                echo "<h1>'$target_rideID'<h1/>";
+
+                $add_created_rides_query = /** @php text */
+                    "INSERT INTO created_rides(driver, ride_id) VALUES('$username', '$target_rideID')";
+
+                $add_created_rides_result = pg_query($dbconn, $add_created_rides_query);
+
+            }
+
+        }
+    ?>
 </html>
 
