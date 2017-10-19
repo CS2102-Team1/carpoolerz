e9b6390c745b98765ed9f8246589834004f8931d
Derian Tungka <balibone@gmail.com>
2017-10-20 01:11:05 +0800

    [SCHEMA] Added working data population scripts for all tables
    Also removed Setup.sql because admin, driver and user datas are all populated in systemuser.sql script. Please read comments in each script. This shit took quite some time to do :D

--- sql/Setup.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO systemuser (username, fullname, password, licensenum) values ('admin', 'Admin', 'admin', '1423123422');
-INSERT INTO systemuser (username, fullname, password, licensenum) values ('rider', 'Dummy Rider', 'rider', NULL);
-INSERT INTO systemuser (username, fullname, password, licensenum) values ('driver', 'Mr GrabnUber', 'driver', '1423123421');
