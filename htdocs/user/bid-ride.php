eb24556554af186bd1996e35362ee048d15d92f3
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-23 23:45:46 +0800

    [FORMAT] Added formatting changes to login page for consistency and added underlying template for bid-ride.php

--- htdocs/user/bid-ride.php
+++ htdocs/user/bid-ride.php
@@ -1,70 +1,79 @@
 <?php
-session_start();
+    session_start();
 
-$username = $_SESSION['username'];
-$password = $_SESSION['password'];
+    $username = $_SESSION['username'];
+    $password = $_SESSION['password'];
 
-$dbconn = pg_connect("host=localhost port=5432 dbname=carpoolerz user=postgres password=postgres")
-or die('Could not connect: ' . pg_last_error());
+    $dbconn = pg_connect("host=localhost port=5432 dbname=carpoolerz user=postgres password=postgres")
+    or die('Could not connect: ' . pg_last_error());
 
-$query = /** @lang text */
-    "SELECT * FROM systemuser WHERE username = '$username' AND password = '$password'";
+    $query = /** @lang text */
+        "SELECT * FROM systemuser WHERE username = '$username' AND password = '$password'";
 
-$result = pg_query($dbconn, $query);
+    $result = pg_query($dbconn, $query);
 
-if (pg_num_rows($result) == 0) {
-    header("Location: ../login.php");
-}
+    if (pg_num_rows($result) == 0) {
+        header("Location: ../login.php");
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <?php include '../header.shtml'; ?>
     <link href="../main.css" rel="stylesheet" />
 </head>
 
 <body>
+    <?php include 'navbar-user.shtml'; ?>
 
-<?php include 'navbar-user.shtml'; ?>
+    <div class="container">
+        <div class="container-fluid">
+            <br/>
+            <form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="search-form">
+                <div class="row">
+                    <div class="form-group col-6">
+                        <label for="p_start">Starting Address: </label>
+                        <input type="text" name="p_start" class="form-control" id="s_start_address" placeholder="Enter Starting Address"/>
+                    </div>
 
-<div class=container>
+                    <div class="form-group col-6">
+                        <label for="p_end">Car Number Plate: </label>
+                        <input type="text" name="p_end" class="form-control" id="s_end_address" placeholder="Enter Destination Address"/>
+                    </div>
+                </div>
+                <button type="submit" name="searchRidesTrigger" class="form-control btn btn-primary">SEARCH RIDES</button>
+                <br/>
+            </form>
+        </div>
+    </div>
 
-    <div class=container>
-        <!-- Display all current driver offered rides -->
-        <table class="table table-striped table-hover">
-            <thead class="thead-inverse">
-            <tr>
-                <th>Ride ID</th>
-                <th>Start Location</th>
-                <th>End Location</th>
-                <th>Start Time/Date</th>
-                <th>End Time/Date</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $query = 'SELECT * FROM ride';
-            $result = pg_query($query);
-            while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    <?php
+
+        if (isset($_POST['searchRidesTrigger'])) {
+
+            $start = $_POST['p_start'];
+            $end = $_POST['p_end'];
+
+            // Get relevant data from rides table
+            $ride_matches_query = /** @php text */
+                    "SELECT * FROM ride WHERE from_address LIKE '%'$start'%' AND to_address LIKE '%'$end'%' AND end_time IS NULL";
+
+            $ride_matches_result = pg_query($dbconn, $ride_matches_query);
+            echo "Hello world";
+            while ($line = pg_fetch_array($ride_matches_result, null, PGSQL_ASSOC)) {
+                echo "Hello world";
                 echo "\t<tr>\n";
                 foreach ($line as $col_value) {
+                    echo "Hello world in for each loop";
                     echo "\t\t<td>$col_value</td>\n";
                 }
                 echo "\t</tr>\n";
             }
-            ?>
-            </tbody>
-        </table>
 
-        <br/>
+        }
 
+    ?>
 
-
-    </div>
-
-</div>
-
-<?php include '../footer.shtml'; ?>
+    <?php include '../footer.shtml'; ?>
 </body>
