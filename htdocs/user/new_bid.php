a8b2fe757c44b206aa53d7571df5bcc0232e8b94
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-10-25 18:02:21 +0800

    [NEWBID] Setup dummy template for new bids page

--- htdocs/user/new_bid.php
+++ htdocs/user/new_bid.php
@@ -15,6 +15,38 @@
     if (pg_num_rows($result) == 0) {
         header("Location: ../login.php");
     }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../header.shtml'; ?>
+    <link href="../main.css" rel="stylesheet" />
+</head>
+
+<body>
+<?php include 'navbar-user.shtml'; ?>
+
+<div class="container">
+    <div class="container-fluid">
+        <br/>
+        <form class="row" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="search-form">
+            <div class="form-group col-6">
+                <label for="p_start">Starting Address: </label>
+                <input type="text" name="p_start" class="form-control" id="s_start_address" placeholder="Enter Starting Address"/>
+            </div>
+
+            <div class="form-group col-6">
+                <label for="p_end">Car Number Plate: </label>
+                <input type="text" name="p_end" class="form-control" id="s_end_address" placeholder="Enter Destination Address"/>
+            </div>
+            <button type="submit" name="searchRidesTrigger" class="form-control btn btn-primary">SEARCH RIDES</button>
+            <br/>
+        </form>
+    </div>
+</div>
+
+<?php
 
     $target_rideID = null;
 
@@ -24,5 +56,8 @@
     } else {
         echo "<br/><h1 class='text-center'>Ride ID Invalid<h1/>";
     }
+
 ?>
 
+<?php include '../footer.shtml'; ?>
+</body>
