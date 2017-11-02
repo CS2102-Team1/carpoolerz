0a2b6cd8c0cdb12999e24ca353d70a84afdc59cc
Charles Goh <charles.goh@live.com>
2017-11-02 10:23:12 +0800

    [CREATEBID] Implementation for create bids for admin site complete

--- htdocs/admin/create_bid.php
+++ htdocs/admin/create_bid.php
@@ -3,74 +3,83 @@
 	or die('Could not connect: ' . pg_last_error());
 	
 	// Define variables and initialize with empty values
-	$username = $name = $password = $licensenum = $is_admin = "";
-	$username_err= $name_err = $password_err= $licensenum_err = $is_admin_err = "";
-	
+	$username = $ride_id = $bid_amount = $is_admin = "";
+	$username_err = $ride_id_err = $amount_err = "";
+	$highest_bid = '0';
+
 	// Processing form data when form is submitted
-	if($_SERVER["REQUEST_METHOD"] == "POST"){
-		// Validate username
-		$input_username = trim($_POST["username"]);
-		if(empty($input_username)){
-			$username_err = "Please enter a username.";
-			} /*elseif(!filter_var(trim($_POST["name"]), FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z'-.\s ]+$/")))){
-			$name_err = 'Please enter a valid username.';
-			} */else{
-			$username = $input_username;
-		}
-		
-		// Validate full name
-		$input_name = trim($_POST["name"]);
-		if(empty($input_name)){
-			$name_err = "Please enter a name.";
-			} elseif(!filter_var(trim($_POST["name"]), FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z'-.\s ]+$/")))){
-			$name_err = 'Please enter a valid name.';
-			} else{
-			$name = $input_name;
-		}
-		
-		// Validate password
-		$input_pw = trim($_POST["password"]);
-		if(empty($input_pw)){
-			$password_err = 'Please enter a password.';     
-			} else{
-			$password = $input_pw;
-		}
-		
-		// Validate license number
-		$input_ln = trim($_POST["licensenum"]);
-		if(empty($input_ln)){
-			$licensenum_err = 'Please enter a license number.';     
-			} else{
-			$licensenum = $input_ln;
-		}
-		
-		// Validate is_admin
-		$input_is_admin = trim($_POST["is_admin"]);
-		if(empty($input_is_admin)){
-			$is_admin_err = "Please specify admin priviliges (Y/N)";     
-			} elseif($input_is_admin !== "Y" AND $input_is_admin !== "N" AND $input_is_admin !== "t" AND $input_is_admin !== "f" AND $input_is_admin !== "TRUE" AND $input_is_admin !== "FALSE"){
-			$is_admin_err = 'Please enter Y or N';
-			} else{
-			if($input_is_admin === "Y" OR $input_is_admin === "t" OR $input_is_admin === "TRUE"){
-				$is_admin = "TRUE";
-				}else if($input_is_admin === "N" OR $input_is_admin === "f" OR $input_is_admin === "FALSE"){
-				$is_admin = "FALSE";
-			}
-		}
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $input_ride_id = trim($_POST["ride_id"]);
+        $input_bid_amount = trim($_POST["bid_amount"]);
+        $input_username = trim($_POST["username"]);
+
+        // Validate username
+        $check_bid_exists_query = /**@php text */
+            "SELECT *
+            FROM bid
+            WHERE ride_id = '$input_ride_id' 
+            AND passenger = '$input_username'";
+
+        $check_bid_exists_result = pg_query($dbconn, $check_bid_exists_query);
+        if (empty($input_username)) {
+            $username_err = "Please enter a username.";
+        } elseif (pg_num_rows($check_bid_exists_result) != 0) {
+            $username_err = "Bid already exists. Use update bid page instead";
+        } else {
+            $username = $input_username;
+        }
+
+        // Validate ride id: Assumption that input types are correct, e.g. numeric is numeric not alphabets
+        $check_ride_id_query = /**@php text */
+            "SELECT ride_id, highest_bid
+            FROM ride WHERE ride_id = '$input_ride_id'";
+        $check_ride_id_result = pg_query($dbconn, $check_ride_id_query);
+        $check_ride_id_array = pg_fetch_row($check_ride_id_result, null, PGSQL_BOTH);
+        if (empty($input_ride_id)) {
+            $ride_id_err = "Please enter a ride ID.";
+        } elseif (pg_num_rows($check_ride_id_result) == 0) {
+            $ride_id_err = "Ride ID does not exist";
+        } else {
+            $highest_bid = $check_ride_id_array['highest_bid'];
+            $ride_id = $input_ride_id;
+        }
+
+        // Validate bid amount
+        if (empty($input_bid_amount)) {
+            $amount_err = "Please enter a bid amount.";
+        } elseif ($highest_bid >= $input_bid_amount) {
+            $amount_err = "Please enter a higher bid amount";
+        } else {
+            $bid_amount = $input_bid_amount;
+        }
 		
 		// Check input errors before inserting in database
-		if(empty($username_err) && empty($name_err) && empty($password_err) && empty($licensenum_err) && empty($is_admin_err)){
-			$sql = "INSERT into systemuser VALUES('$username', '$name', '$password', '$licensenum', '$is_admin')";
-			
-			$result = pg_query($dbconn, $sql);
-			
-			if(!$result){
-				echo pg_last_error($dbconn);
-				} else {
-				echo "<h3>User Created successfully</h3>"."<br>";
-				echo "<h4>Redirecting you back to View Users page</h4>";
-				header("refresh:4;url=admin-users.php");
-			} 
+		if(empty($username_err) && empty($ride_id_err) && empty($amount_err)){
+            // Check if bid has already been placed for user
+            $already_placed_bid_query = /** @php text */
+                    "SELECT r.ride_id, r.highest_bid, b.amount, b.passenger 
+                    FROM ride r, bid b
+                    WHERE r.ride_id = b.ride_id
+                    AND r.ride_id = '$input_ride_id' AND r.passenger = '$input_username'";
+            $result = pg_query($dbconn, $already_placed_bid_query);
+
+            if (pg_num_rows($result) == 0) {
+                $insert_bid_query = /** @php text */
+                        "INSERT INTO bid(amount, ride_id, passenger) 
+                        VALUES ('$input_bid_amount', '$input_ride_id', '$input_username')";
+
+                pg_query($insert_bid_query);
+
+                $update_highest_bid_query = /**@php text*/
+                        "UPDATE ride SET highest_bid = '$input_bid_amount', passenger = '$input_username'
+                        WHERE ride_id = '$ride_id'";
+
+                pg_query($update_highest_bid_query);
+
+                echo "<h3 class='text-center'>Bid Created successfully</h3>" . "<br>";
+                echo "<h4 class='text-center'>Redirecting you back to Bids page</h4>";
+                header("refresh:4;url=admin-bids.php");
+            }
 		}
 	}
 ?>
@@ -94,38 +103,28 @@
 				<div class="row">
 					<div class="col-md-12">
 						<div class="page-header">
-							<h2>Create User</h2>
+							<h2>Create Bid</h2>
 						</div>
-						<p>Please fill this form and submit to add user to the database.</p>
+						<p>Please fill this form and submit to add a bid for a user into the database.</p>
 						<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-							<div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
-								<label>Full Name</label>
-								<input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
-								<span class="help-block"><?php echo $name_err;?></span>
-							</div>
 							<div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
 								<label>Username</label>
 								<input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
 								<span class="help-block"><?php echo $username_err;?></span>
 							</div>
-							<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-								<label>Password</label>
-								<input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-								<span class="help-block"><?php echo $password_err;?></span>
+							<div class="form-group <?php echo (!empty($ride_id_err)) ? 'has-error' : ''; ?>">
+								<label>Ride ID</label>
+								<input type="text" name="ride_id" class="form-control" value="<?php echo $ride_id; ?>">
+								<span class="help-block"><?php echo $ride_id_err;?></span>
 							</div>
-							<div class="form-group <?php echo (!empty($licensenum_err)) ? 'has-error' : ''; ?>">
-								<label>License Number</label>
-								<input type="text" name="licensenum" class="form-control" value="<?php echo $licensenum; ?>">
-								<span class="help-block"><?php echo $licensenum_err;?></span>
-							</div>
-							<div class="form-group <?php echo (!empty($is_admin_err)) ? 'has-error' : ''; ?>">
-								<label>Admin Priviliges (Y/N)</label>
-								<input type="text" name="is_admin" class="form-control" value="<?php echo $is_admin; ?>">
-								<span class="help-block"><?php echo $is_admin_err;?></span>
-							</div>
+                            <div class="form-group <?php echo (!empty($amount_err)) ? 'has-error' : ''; ?>">
+                                <label>Bid Amount</label>
+                                <input type="text" name="bid_amount" class="form-control" value="<?php echo $bid_amount; ?>">
+                                <span class="help-block"><?php echo $amount_err;?></span>
+                            </div>
 							<input type="submit" class="btn btn-primary" value="Submit">
 							<input type="reset" class="btn btn-warning" value="Reset">
-							<a href="admin-users.php" class="btn btn-default">Go Back To User Page</a>
+							<a href="admin-bids.php" class="btn btn-default">Go Back To Bids Page</a>
 						</form>
 					</div>
 				</div>        
